2d210326fa1f99c688acb0e823b218a0194a0a1b
MrRacoonGH <hadrien181107@gmail.com>
2025-02-05 09:14:30 +0100

    update V1.0

---
+++ js/app.js
@@ -0,0 +1,13 @@
+console.log("====")
+console.log("====")
+console.log("====")
+
+
+const $playerSelect = document.querySelector(".new-game-screen-pick-player-icon--selected")
+
+console.log($playerSelect)
+
+
+$element.classList.add("new-game-screen-pick-player-icon--selected")  {
+
+}
